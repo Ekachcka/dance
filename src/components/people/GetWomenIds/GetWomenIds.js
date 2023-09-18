90dc0f2a6b3bfa9697951bafd50c848f2ccd7b89
Ekachcka <2331286yan@gmail.com>
2023-09-18 13:21:02 +0300

    do sort

--- src/components/people/GetWomenIds/GetWomenIds.js
+++ src/components/people/GetWomenIds/GetWomenIds.js
@@ -1,55 +1,13 @@
 import React, { useState } from "react";
-import peopleData from "../people.json";
+import doSort from "../../../functions/doSort/doSort";
 import GetWomanIdS from "../GetWomanIdS/GetWomanIdS";
 function GetWomenIds({ id }) {
   const [Sort, setSort] = useState("id");
-  const arr = [];
-  for (let i = 0; i < peopleData.woman.length; i++) {
-    for (let j = 0; j < peopleData.man[id].partner.length; j++) {
-      if (i == peopleData.man[id].partner[j]) {
-        arr.push(peopleData.woman[i]);
-      }
-    }
-  }
-  for (let i = 0; i < peopleData.woman.length; i++) {
-    if (Sort == "id") {
-      arr.sort((a, b) => {
-        if (a.id < b.id) {
-          return 1;
-        }
-        if (a.id > b.id) {
-          return -1;
-        }
-      });
-      console.log(arr);
-    }
-    if (Sort == "age") {
-      arr.sort((a, b) => {
-        if (a.age < b.age) {
-          return 1;
-        }
-        if (a.age > b.age) {
-          return -1;
-        }
-      });
-      console.log(arr);
-    }
-    if (Sort == "name") {
-      arr.sort((a, b) => {
-        if (a.name > b.name) {
-          return 1;
-        }
-        if (a.name < b.name) {
-          return -1;
-        }
-      });
-      console.log(arr);
-    }
-  }
+
   return (
     <>
       <ul>
-        {arr.map((i) => (
+        {doSort(Sort, id).map((i) => (
           <li key={i.id}>
             <GetWomanIdS id={i.id} />
           </li>
---
+++ src/functions/doSort/doSort.js
@@ -0,0 +1,49 @@
+import peopleData from "../../components/people/people.json";
+function doSort(Sort, id) {
+  const arr = [];
+  for (let i = 0; i < peopleData.woman.length; i++) {
+    for (let j = 0; j < peopleData.man[id].partner.length; j++) {
+      if (i == peopleData.man[id].partner[j]) {
+        arr.push(peopleData.woman[i]);
+      }
+    }
+  }
+  for (let i = 0; i < peopleData.woman.length; i++) {
+    if (Sort == "id") {
+      arr.sort((a, b) => {
+        if (a.id < b.id) {
+          return 1;
+        }
+        if (a.id > b.id) {
+          return -1;
+        }
+      });
+      console.log(arr);
+    }
+    if (Sort == "age") {
+      arr.sort((a, b) => {
+        if (a.age < b.age) {
+          return 1;
+        }
+        if (a.age > b.age) {
+          return -1;
+        }
+      });
+      console.log(arr);
+    }
+    if (Sort == "name") {
+      arr.sort((a, b) => {
+        if (a.name > b.name) {
+          return 1;
+        }
+        if (a.name < b.name) {
+          return -1;
+        }
+      });
+      console.log(arr);
+    }
+  }
+  return arr;
+}
+
+export default doSort;
