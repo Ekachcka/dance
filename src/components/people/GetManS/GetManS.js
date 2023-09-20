530eb40c4bb77186a392f78a09d31df812433969
Ekachcka <2331286yan@gmail.com>
2023-09-21 00:29:08 +0300

    Add age list

--- src/components/people/GetManS/GetManS.js
+++ src/components/people/GetManS/GetManS.js
@@ -4,11 +4,17 @@ import { useState } from "react";
 import { Listbox } from "@headlessui/react";
 
 const people = [
-  { id: 0, name: "Man", index: "M", unavailable: false },
-  { id: 1, name: "Woman", index: "W", unavailable: false },
+  { id: 0, name: "Man", index: "M" },
+  { id: 1, name: "Woman", index: "W" },
 ];
+const old = [
+  { id: 0, name: "<=25", index: "Y" },
+  { id: 1, name: ">25", index: "O" },
+];
+let arr = [];
 function GetManS() {
   const [Sex, setSex] = useState(people[0]);
+  const [Age, setAge] = useState(old[0]);
 
   let p;
   if (Sex.index == "M") {
@@ -16,13 +22,26 @@ function GetManS() {
   } else if (Sex.index == "W") {
     p = peopleData.woman;
   }
+  arr = [];
+  for (let i = 0; i < p.length; i++) {
+    if (Age.index == "Y") {
+      if (p[i].age <= 25) {
+        arr.push(p[i]);
+      }
+    }
+    if (Age.index == "O") {
+      if (p[i].age > 25) {
+        arr.push(p[i]);
+      }
+    }
+  }
 
   return (
     <>
       <div>
         <h1>Choose a man</h1>
         <ul>
-          {p.map((person) => (
+          {arr.map((person) => (
             <li key={person.id}>
               <a href={`/${Sex.index}/${person.id}`}>{person.name}</a>
             </li>
@@ -32,11 +51,17 @@ function GetManS() {
           <Listbox.Button>{Sex.name}</Listbox.Button>
           <Listbox.Options>
             {people.map((person) => (
-              <Listbox.Option
-                key={person.id}
-                value={person}
-                disabled={person.unavailable}
-              >
+              <Listbox.Option key={person.id} value={person}>
+                {person.name}
+              </Listbox.Option>
+            ))}
+          </Listbox.Options>
+        </Listbox>
+        <Listbox value={Age} onChange={setAge}>
+          <Listbox.Button>{Age.name}</Listbox.Button>
+          <Listbox.Options>
+            {old.map((person) => (
+              <Listbox.Option key={person.id} value={person}>
                 {person.name}
               </Listbox.Option>
             ))}
