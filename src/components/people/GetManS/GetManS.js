3557300e6c17025284a3dedfb60fe76d98590057
Ekachcka <2331286yan@gmail.com>
2023-09-21 00:18:19 +0300

    Add listBox

--- src/components/people/GetManS/GetManS.js
+++ src/components/people/GetManS/GetManS.js
@@ -1,17 +1,47 @@
 import React from "react";
 import peopleData from "../people.json";
+import { useState } from "react";
+import { Listbox } from "@headlessui/react";
+
+const people = [
+  { id: 0, name: "Man", index: "M", unavailable: false },
+  { id: 1, name: "Woman", index: "W", unavailable: false },
+];
 function GetManS() {
+  const [Sex, setSex] = useState(people[0]);
+
+  let p;
+  if (Sex.index == "M") {
+    p = peopleData.man;
+  } else if (Sex.index == "W") {
+    p = peopleData.woman;
+  }
+
   return (
     <>
       <div>
         <h1>Choose a man</h1>
         <ul>
-          {peopleData.man.map((person) => (
+          {p.map((person) => (
             <li key={person.id}>
-              <a href={`/M/${person.id}`}>{person.name}</a>
+              <a href={`/${Sex.index}/${person.id}`}>{person.name}</a>
             </li>
           ))}
         </ul>
+        <Listbox value={Sex} onChange={setSex}>
+          <Listbox.Button>{Sex.name}</Listbox.Button>
+          <Listbox.Options>
+            {people.map((person) => (
+              <Listbox.Option
+                key={person.id}
+                value={person}
+                disabled={person.unavailable}
+              >
+                {person.name}
+              </Listbox.Option>
+            ))}
+          </Listbox.Options>
+        </Listbox>
       </div>
     </>
   );
