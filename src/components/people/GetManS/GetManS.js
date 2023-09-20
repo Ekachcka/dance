ef02524c8ba95a62fc8912b424b449cc5d0f4e86
Ekachcka <2331286yan@gmail.com>
2023-09-21 00:54:29 +0300

    Add all to list

--- src/components/people/GetManS/GetManS.js
+++ src/components/people/GetManS/GetManS.js
@@ -6,34 +6,41 @@ import { Listbox } from "@headlessui/react";
 const people = [
   { id: 0, name: "Man", index: "M" },
   { id: 1, name: "Woman", index: "W" },
+  { id: 3, name: "All", index: "A" },
 ];
 const old = [
   { id: 0, name: "<=25", index: "Y" },
   { id: 1, name: ">25", index: "O" },
+  { id: 3, name: "All", index: "A" },
 ];
 let arr = [];
 function GetManS() {
   const [Sex, setSex] = useState(people[0]);
   const [Age, setAge] = useState(old[0]);
-
-  let p;
+  let p = [];
   if (Sex.index == "M") {
     p = peopleData.man;
   } else if (Sex.index == "W") {
     p = peopleData.woman;
+  } else if (Sex.index == "A") {
+    p = peopleData.man.concat(peopleData.woman);
   }
-  arr = [];
-  for (let i = 0; i < p.length; i++) {
-    if (Age.index == "Y") {
-      if (p[i].age <= 25) {
-        arr.push(p[i]);
+  if (Age.index != "A") {
+    arr = [];
+    for (let i = 0; i < p.length; i++) {
+      if (Age.index == "Y") {
+        if (p[i].age <= 25) {
+          arr.push(p[i]);
+        }
+      }
+      if (Age.index == "O") {
+        if (p[i].age > 25) {
+          arr.push(p[i]);
+        }
       }
     }
-    if (Age.index == "O") {
-      if (p[i].age > 25) {
-        arr.push(p[i]);
-      }
-    }
+  } else {
+    arr = p;
   }
 
   return (
@@ -43,7 +50,13 @@ function GetManS() {
         <ul>
           {arr.map((person) => (
             <li key={person.id}>
-              <a href={`/${Sex.index}/${person.id}`}>{person.name}</a>
+              <a
+                href={`/${person.partner != undefined ? "M" : "W"}/${
+                  person.id
+                }`}
+              >
+                {person.name}
+              </a>
             </li>
           ))}
         </ul>
